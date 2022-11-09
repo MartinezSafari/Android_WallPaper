2ef39d40a049a9c9d4f42cf0e5183cfa311528d1
martinez <martinez@thereachtrust.org>
2022-11-09 22:05:42 +0200

    create fragments and functions class

--- app/src/main/java/org/thereachtrust/wallpaper/MainActivity.java
+++ app/src/main/java/org/thereachtrust/wallpaper/MainActivity.java
@@ -17,7 +17,11 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.thereachtrust.wallpaper.Utils.Functions;
 import org.thereachtrust.wallpaper.databinding.ActivityMainBinding;
+import org.thereachtrust.wallpaper.ui.collections.CollectionsFragment;
+import org.thereachtrust.wallpaper.ui.favorite.FavoriteFragment;
+import org.thereachtrust.wallpaper.ui.photo.PhotoFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,9 +45,12 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_photo, R.id.nav_collections, R.id.nav_favorite)
                 .setOpenableLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavController navController = Navigation.findNavController(this, R.id.main_container);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        //PhotoFragment photoFragment= new PhotoFragment();
+        //Functions.changeMainFragment(MainActivity.this, photoFragment);
     }
 
     @Override
@@ -55,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavController navController = Navigation.findNavController(this, R.id.main_container);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
@@ -76,12 +83,18 @@ public class MainActivity extends AppCompatActivity {
         int id= item.getItemId();
         if (id==R.id.nav_photo){
             //Handle the photo action
+            PhotoFragment photoFragment= new PhotoFragment();
+            Functions.changeMainFragment(MainActivity.this, photoFragment);
         }
         else if(id==R.id.nav_collections){
             //Handle the collections action
+            CollectionsFragment collectionsFragment= new CollectionsFragment();
+            Functions.changeMainFragment(MainActivity.this, collectionsFragment);
         }
         else if(id==R.id.nav_favorite){
             //Handle the favorite action
+            FavoriteFragment favoriteFragment= new FavoriteFragment();
+            Functions.changeMainFragment(MainActivity.this, favoriteFragment);
         }
         DrawerLayout drawer =(DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
